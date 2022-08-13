e08e1454fb30dd6d206b29d4804cc151b5aada7d
hitohito1023 <>
2022-08-13 15:58:28 +0900

    [finish]lesson8

--- app/src/main/java/com/example/gametraining/ResultActivity.java
+++ app/src/main/java/com/example/gametraining/ResultActivity.java
@@ -2,7 +2,13 @@ package com.example.gametraining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.shadow.ShadowRenderer;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView scoreLabel = findViewById(R.id.scoreLabel);
+        TextView highScoreLabel = findViewById(R.id.highSoreLabel);
+
+        int score = getIntent().getIntExtra("SCORE", 0);
+        scoreLabel.setText(score + "");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("GAME_DATA", MODE_PRIVATE);
+        int highScore = sharedPreferences.getInt("HIGH_SCORE", 0);
+
+        if (score > highScore) {
+            highScoreLabel.setText("High Score : " + score);
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("HIGH_SCORE", score);
+            editor.apply();
+        } else {
+            highScoreLabel.setText("High Score : " + highScore);
+        }
+    }
+
+    public void tryAgain(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
